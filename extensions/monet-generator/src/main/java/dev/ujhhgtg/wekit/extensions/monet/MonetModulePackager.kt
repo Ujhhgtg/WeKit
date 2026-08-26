8b1aa1bde5a2d27f4e454067e618e126733bb498
Ujhhgtg <feyxiexzf@gmail.com>
2026-08-26 14:45:53 +0800

    fix(monet): preserve requested hot-install signal

--- extensions/monet-generator/src/main/java/dev/ujhhgtg/wekit/extensions/monet/MonetModulePackager.kt
+++ extensions/monet-generator/src/main/java/dev/ujhhgtg/wekit/extensions/monet/MonetModulePackager.kt
@@ -79,6 +79,9 @@ restore_overlays() {
     private const val CUSTOMIZE_SCRIPT = $$"""# shellcheck disable=SC2034
 SKIPUNZIP=0
 
+# Ask compatible root managers to activate this module update immediately.
+export MODULE_HOT_INSTALL_REQUEST=true
+
 ui_print " "
 ui_print '             _       __     __ __ _ __'
 ui_print '            | |     / /__  / //_/(_) /_'
