e17ef799729403ec281e47b8314542c869478263
Ujhhgtg <feyxiexzf@gmail.com>
2026-08-29 03:05:18 +0800

    feat: improve httpx -> httpx2 update guide

--- extensions/python-runtime/runtime/src/main/python/httpx/__init__.py
+++ extensions/python-runtime/runtime/src/main/python/httpx/__init__.py
@@ -1,8 +1,8 @@
 raise ImportError(
     """The `httpx` package has been replaced by `httpx2` in this runtime.
-`httpx2` is a drop-in replacement, so you can migrate by changing imports from `httpx` to `httpx2`.
+`httpx2` is a drop-in replacement, so you can migrate by simply changing imports from `httpx` to `httpx2`.
 
-For process-wide compatibility with code which still imports `httpx`, run this before importing that code:
+For process-wide compatibility with code which still imports `httpx`, you can *instead* run this before importing that code:
 
     import httpx2
     httpx2.alias_httpx()
